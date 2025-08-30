5b275375c3308dfe7e0412ee8b3ab7a52eaad65a
Norm Meyer <normjmeyer@gmail.com>
2025-08-29 21:08:30 -0500

    Added cart chap 9

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -53,6 +53,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_raises(ActiveRecord::RecordNotDestroyed) do
+      delete product_url(products(:two))
+    end
+
+    assert Product.exists?(products(:two).id)
+  end
+
   test "should destroy product" do
     assert_difference("Product.count", -1) do
       delete product_url(@product)
